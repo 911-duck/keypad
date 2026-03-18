f2ddb2c30d4d43fa595444a6e4d0e9056d0d2fbe
911-duck <zhenyabiruk800@gmail.com>
2026-03-18 12:00:11 +0300

    Update keypad.h

--- keypad/keypad.h
+++ keypad/keypad.h
@@ -1,14 +1,21 @@
+// класс - Keypad
+// методы :
+// .read(int h, int w);
+// .readChar();
+// вызов : 
+// Keypad "name" = Keypad(int pinsA[4], int pinsB[4]);
+
 #pragma once
 
 class Keypad {
   public:
-  Keypad(int pinsA[4], int pinsB[4]);
-  int read(int h, int w);
-  char readChar();
+  Keypad(int pinsA[4], int pinsB[4]); // конструктор класса
+  int read(int h, int w); // чтение нажатой кнопки h и w - это область виденья
+  char readChar(); // чтение символа
   private:
-  int _pinsA[4];
+  int _pinsA[4]; // пины матрицы
   int _pinsB[4];
-  const char _KEYS_TO_CHAR[9][4]={
+  const char _KEYS_TO_CHAR[9][4]={ // символы
     {'1','A','B','C'},
     {'2','D','E','F'},
     {'3','G','H','I'},
@@ -19,7 +26,7 @@ class Keypad {
     {'8','V','W','X'},
     {'9','Y','Z','.'},
     {'*','+','-','$'},
-    {'0',' ','<','/'}, // < удаление
+    {'0',' ','<','/'}, // '<' - удаление, ' ' - пробел
     {'@','#',';',':'},
   };
 };
