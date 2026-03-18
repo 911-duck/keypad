b202c7af59d9e519200d90ab1f7cfd5f98162f10
911-duck <zhenyabiruk800@gmail.com>
2026-03-18 12:09:10 +0300

    Update keypad.ino

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -1,3 +1,6 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h> // подключение библеотек для использования жк дислея
+
 #include "keypad.h" // класс для клавиатуры
 
 const int pinsKeypad[2][4] = {
@@ -5,10 +8,14 @@ const int pinsKeypad[2][4] = {
   {5,4,3,2}
 }; // пины матрицы клавиатуры 
 
-Keypad keypad = Keypad(pinsKeypad[0], pinsKeypad[1]); // вызов
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+Keypad keypad = Keypad(pinsKeypad[0], pinsKeypad[1]); // создание инстансов классов
 
 void setup() {
   Serial.begin(9600); // для откладки
+
+  lcd.init(); 
+  lcd.backlight(); // иницализация жк дисплея   
 }
 
 String output = ""; // результат
@@ -21,6 +28,10 @@ void loop() {
     Serial.println("");
     Serial.println(output);
     Serial.println("");
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(output); // вывод на дисплей результат
   }
 
   if(n == '<'){ // удаление
