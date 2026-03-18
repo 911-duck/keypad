c9782b2fc148153760e8cc4c8866a24909f1686a
911-duck <zhenyabiruk800@gmail.com>
2026-03-18 22:02:01 +0300

    Update keypad.ino

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -1,40 +1,47 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h> // подключение библеотек для использования жк дислея
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>  // подключение библеотек для использования жк дислея
 
-#include "keypad.h" // класс для клавиатуры
+#include "keypad.h"  // класс для клавиатуры
 
 const int pinsKeypad[2][4] = {
-  {6,7,8,9},
-  {5,4,3,2}
-}; // пины матрицы клавиатуры 
+  { 6, 7, 8, 9 },
+  { 5, 4, 3, 2 }
+};  // пины матрицы клавиатуры
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
-Keypad keypad = Keypad(pinsKeypad[0], pinsKeypad[1]); // создание инстансов классов
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+Keypad keypad = Keypad(pinsKeypad[0], pinsKeypad[1]);  // создание инстансов классов
 
 void setup() {
-  Serial.begin(9600); // для откладки
+  Serial.begin(9600);  // для откладки
 
-  lcd.init(); 
-  lcd.backlight(); // иницализация жк дисплея   
+  lcd.init();
+  lcd.backlight();  // иницализация жк дисплея
 }
 
-String output = ""; // результат
+String output = "";  // результат
+String newS = "";
 
 void loop() {
-  char n = keypad.readChar(); // получаем символ
-  
-  if(n != '_' && n != '<'){ // если разрешённый символ
-    output+=n;
+  char n = keypad.readChar();  // получаем символ
+
+  if (n != '_' && n != '<') {  // если разрешённый символ
+    output += n;
     Serial.println("");
     Serial.println(output);
     Serial.println("");
 
     lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(output); // вывод на дисплей результат
   }
 
-  if(n == '<'){ // удаление
-    output.remove(output.length()-1);
+  if (n == '<') {  // удаление
+    newS = "";
+    for(int i = 0; i < output.length()-1; i++){
+      newS += output[i];
+    }
+    output = newS;
+    lcd.clear();
   }
+  
+  lcd.setCursor(0, 0);
+  lcd.print(output);  // вывод на дисплей результат
 }
