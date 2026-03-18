6dabe649c4e23513d8a5ffddc26a3b03adeb5141
911-duck <zhenyabiruk800@gmail.com>
2026-03-18 11:54:44 +0300

    Update keypad.ino

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -1,29 +1,29 @@
-#include "keypad.h"
+#include "keypad.h" // класс для клавиатуры
 
 const int pinsKeypad[2][4] = {
   {6,7,8,9},
   {5,4,3,2}
-};
+}; // пины матрицы клавиатуры 
 
-Keypad keypad = Keypad(pinsKeypad[0], pinsKeypad[1]);
+Keypad keypad = Keypad(pinsKeypad[0], pinsKeypad[1]); // вызов
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // для откладки
 }
 
-String output = "";
+String output = ""; // результат
 
 void loop() {
-  char n = keypad.readChar();
+  char n = keypad.readChar(); // получаем символ
   
-  if(n != '_' && n != '<'){
+  if(n != '_' && n != '<'){ // если разрешённый символ
     output+=n;
     Serial.println("");
     Serial.println(output);
     Serial.println("");
   }
 
-  if(n == '<'){
+  if(n == '<'){ // удаление
     output.remove(output.length()-1);
   }
 }
